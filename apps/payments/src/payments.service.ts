81ad362e369ee67723612adb3fdb12358d509eb0
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-07-01 14:09:57 -0300

    fix: create charge by communicating with payments microservice during reservation creation

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -7,25 +7,18 @@ import { CreateChargeDto } from '@app/common/dto/create-charge.dto';
 export class PaymentsService {
   private readonly stripe = new Stripe(
     this.configService.get('STRIPE_SECRET_KEY'),
-    {
-      apiVersion: '2024-06-20',
-    },
+    { apiVersion: '2024-06-20' },
   );
 
   constructor(private readonly configService: ConfigService) {}
 
-  async createCharge({ card, amount }: CreateChargeDto) {
-    const paymentMethod = await this.stripe.paymentMethods.create({
-      type: 'card',
-      card,
-    });
-
+  async createCharge({ amount }: CreateChargeDto) {
     return this.stripe.paymentIntents.create({
-      payment_method: paymentMethod.id,
       amount: amount * 100,
       confirm: true,
-      payment_method_types: ['card'],
       currency: 'usd',
+      payment_method: 'pm_card_visa',
+      return_url: 'https://test.com',
     });
   }
 }
