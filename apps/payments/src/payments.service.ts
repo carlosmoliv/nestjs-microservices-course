5bafefef59cb49667ed518ccbe83b780d9356f2a
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-07-02 09:51:17 -0300

    feat(payments): emit notify_email event on charge creation

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import Stripe from 'stripe';
-import { CreateChargeDto } from '@app/common/dto/create-charge.dto';
+import { NOTIFICATIONS_SERVICE } from '@app/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { PaymentsCreateChargeDto } from './dto/payments-create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
@@ -10,15 +12,21 @@ export class PaymentsService {
     { apiVersion: '2024-06-20' },
   );
 
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(NOTIFICATIONS_SERVICE)
+    private readonly notificationsService: ClientProxy,
+  ) {}
 
-  async createCharge({ amount }: CreateChargeDto) {
-    return this.stripe.paymentIntents.create({
+  async createCharge({ amount, email }: PaymentsCreateChargeDto) {
+    const paymentIntent = await this.stripe.paymentIntents.create({
       amount: amount * 100,
       confirm: true,
       currency: 'usd',
       payment_method: 'pm_card_visa',
       return_url: 'https://test.com',
     });
+    this.notificationsService.emit('notify_email', { email });
+    return paymentIntent;
   }
 }
