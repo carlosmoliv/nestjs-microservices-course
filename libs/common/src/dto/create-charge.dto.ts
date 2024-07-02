fe0d5c9b2634e5ed0b515ab227e0ad4c48d54d54
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-07-02 08:35:36 -0300

    fix: remove credit card inputs from dto

--- libs/common/src/dto/create-charge.dto.ts
+++ libs/common/src/dto/create-charge.dto.ts
@@ -1,19 +1,6 @@
-import { CardDto } from '@app/common/dto/card.dto';
-import {
-  IsDefined,
-  IsNotEmptyObject,
-  IsNumber,
-  ValidateNested,
-} from 'class-validator';
-import { Type } from 'class-transformer';
+import { IsNumber } from 'class-validator';
 
 export class CreateChargeDto {
-  @IsDefined()
-  @IsNotEmptyObject()
-  @ValidateNested()
-  @Type(() => CardDto)
-  card: CardDto;
-
   @IsNumber()
   amount: number;
 }
